6632e1afc87ee23ff66fab2e0b7dd4590a7ff21a
littlefriend <gsh0818@126.com>
2016-04-16 15:26:49 +0800

    first demo

--- tree/search/views.py
+++ tree/search/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+import sys
+import os
 
 # Create your views here.
 
@@ -8,4 +10,16 @@ def hello(request):
     return HttpResponse("hello world")
 
 def search(request):
-    return render_to_response('index.html', {'post': 'result'});
+    if 'q' in request.GET:
+        message = request.GET['q']
+        freq = open('studio/request.txt', 'w')
+        freq.write(message + '\n')
+        freq.close()
+        os.system("./studio/check_serve");
+        fres = open('studio/result.txt', 'r')
+        strlist = fres.readlines()
+        fres.close()
+    else:
+        message = 'Search for a tree'
+        strlist = []
+    return render_to_response('index.html', {'display': 'result', 'message': message, 'strlist': strlist})
