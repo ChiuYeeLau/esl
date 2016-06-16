d221477fe700f787583db011089a3a06b8af8807
littlefriend <gsh0818@126.com>
2016-06-16 18:54:26 +0800

    local version

--- tree/search/util.py
+++ tree/search/util.py
@@ -6,10 +6,13 @@ import re
 import ctypes
 from search.parse import *
 from pymongo import MongoClient
+'''
 client = MongoClient('166.111.139.42')
 db = client.test
 db.authenticate('test', 'test')
 cl = db.syntax
+'''
+cl = MongoClient('127.0.0.1').local.syntax
 check_serve = ctypes.CDLL('./search/check_serve.so')
 
 
