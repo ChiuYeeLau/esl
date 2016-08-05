dd760a5016003e0c85a661374c865cfe58f2f749
zhaoymn <zhaoymn@126.com>
2016-08-06 01:56:38 +0800

    changed to new conjugate
    the interface did not change except for
    if no result then return None
    if got result then return the stem

--- script/conjug.py
+++ script/conjug.py
@@ -2,9 +2,9 @@ import json
 
 def get_conjugate(word):
     word = word.lower()
-    confile = open("conjugate.json")
+    confile = open("newconjugate.json")
     condict = json.loads(confile.read())
     if condict.has_key(word):
         return condict.get(word)
     else:
-        return list()
+        return None
